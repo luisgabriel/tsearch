47b5b59b73965cd89b52f2e8784250b1e5091a25
Luís Gabriel Lima <lampih@gmail.com>
2013-04-21 11:09:30 -0300

    [Sequential] Allow more than one query per execution

--- sequential/src/Main.hs
+++ sequential/src/Main.hs
@@ -11,6 +11,7 @@ import Index as Index
 import Query as Query
 
 printResult :: [(FilePath, Int)] -> IO ()
+printResult [] = putStrLn "No occurrences."
 printResult result = do
     let orderedResult = sortBy (mconcat [flip $ comparing snd, comparing fst]) result
     forM_ orderedResult $ \(filePath, total) ->
@@ -18,7 +19,7 @@ printResult result = do
 
 main :: IO ()
 main = do
-    (path:queryString:_) <- getArgs
+    (path:rawQueries) <- getArgs
     files <- Scanner.getAllFiles path
 
     occurrences <- forM files $ \path -> do
@@ -27,6 +28,10 @@ main = do
         return (path, occurrenceMap)
 
     let indexMap = Index.build occurrences
-    let query = Query.parse queryString
-    let result = Query.perform query indexMap
-    printResult result
+
+    forM_ rawQueries $ \rawQuery -> do
+        let query = Query.parse rawQuery
+        let result = Query.perform query indexMap
+        putStrLn $ "> RESULT for \"" ++ rawQuery ++ "\":"
+        printResult result
+        putStrLn ""
